b254de6308e02f7e6bbe0998d6c7bd38c1a87331
Barysenka <iborisenkoip@gmail.com>
2022-04-29 17:02:03 +0200

    feat: hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function calculateHanoi( disksNumber, turnsSpeed) {
+  let step = Math.pow(2, disksNumber) - 1
+  let time = Math.floor(step * (3600/turnsSpeed))
+
+ const result = {
+   'turns':step,
+   'seconds': time
+ }
+ return result
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSeason(date) {
+  const season = ['spring', 'summer', 'autumn', 'winter']
+  if (arguments.length === 0){
+    return "Unable to determine the time of year!";
+  }else
+   if(date && Date.parse(date) && Date.prototype.toString.call(new Date()) != date){
+
+    // console.log(Date.prototype.getFullYear.call(date))
+    // // console.log(date.getFullYear())
+    // console.log(Date.prototype.getFullYear.call(date)) 
+    // console.log(date.getFullYear()) 
+    let month = date.getMonth()
+    switch (month) {
+      case 2:
+        case 3:
+          case 4:
+        return season[0]
+        case 5:
+          case 6:
+            case 7:
+          return season[1]
+          case 8:
+            case 9:
+              case 10:
+            return season[2]
+            case 0:
+              case 1:
+                case 11:
+              return season[3]
+      
+      default:
+        return 'Invalid date!';
+    }
+
+  }else{
+
+ return 'Invalid date!';
+  }
 }
 
 module.exports = {
